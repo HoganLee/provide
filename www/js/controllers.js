143465c9a1c45df5ab79817722aae68f72e0baa6
Aryaz <aryaz.egh@gmail.com>
2016-03-05 14:14:07 -0700

    Added routing with services

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,69 +1,79 @@
-angular.module('app.controllers', [])
-  
+angular.module('app.controllers', ['app.services'])
+
 .controller('homeCtrl', function($scope) {
 
-})
-   
+        })
+
 .controller('donateCtrl', function($scope) {
 
-})
-   
+        })
+
 .controller('donationDetailsCtrl', function($scope) {
 
-})
-   
+        })
+
 .controller('deliverDonationsCtrl', function($scope) {
 
-})
-   
-.controller('deliveryRequestsCtrl', function($scope, $cordovaGeolocation) {
-    var posOptions = {timeout: 20000, enableHighAccuracy: false};
-    $cordovaGeolocation.getCurrentPosition(posOptions)
-    .then(function (position) {
-        var lat = position.coords.latitude;
-        var lng = position.coords.longitude;
-        var myLatLng = new google.maps.LatLng(lat, lng),
+        })
+
+.controller('deliveryRequestsCtrl', function($scope, $cordovaGeolocation, PickupPoints) {
+        var posOptions = {timeout: 20000, enableHighAccuracy: false};
+        $cordovaGeolocation.getCurrentPosition(posOptions)
+        .then(function (position) {
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+            var myLatLng = new google.maps.LatLng(lat, lng),
             mapOptions = {
                 zoom: 16,
                 center: myLatLng
             },
             map = new google.maps.Map(document.getElementById("map"), mapOptions);
-        var infoWindow = new google.maps.InfoWindow({
-            content: "<div>text <b>goes</b> here</div>"
-        });
-        var marker = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            title: "Me"
-        });
-        marker.addListener('click', function() {
-            infoWindow.open(map, marker);
-        });
-    }, function(err) {
-        // error
-    });
-
+            var infoWindow = new google.maps.InfoWindow({
+                content: "<div>text <b>goes</b> here</div>"
+            });
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: "Me"
+            });
+            marker.addListener('click', function() {
+                infoWindow.open(map, marker);
+            });
+            var directionsService = new google.maps.DirectionsService;
+            var directionsDisplay = new google.maps.DirectionsRenderer;
+            directionsDisplay.setMap(map);
+            directionsService.route({
+                origin: myLatLng,
+                destination: PickupPoints.getDestination(),
+                travelMode: google.maps.TravelMode.DRIVING,
+                waypoints: PickupPoints.getPickupPoints(),
+                optimizeWaypoints: true
+            }, function(response, status) {
+                if (status === google.maps.DirectionsStatus.OK) {
+                    directionsDisplay.setDirections(response);
+                } else {
+                    window.alert('Directions request failed due to ' + status);
+                }
+            }); 
+        })
 })
-   
+
 .controller('pendingDeliveresCtrl', function($scope) {
 
-})
+        })
 
-.controller('MapCtrl', function($scope, $cordovaGeolocation) {
-})
-   
 .controller('donationDeliveryCtrl', function($scope) {
 
-})
-   
+        })
+
 .controller('notesCtrl', function($scope) {
 
-})
-   
+        })
+
 .controller('congratulationsCtrl', function($scope) {
 
-})
-   
+        })
+
 .controller('congratulations2Ctrl', function($scope) {
 
-})
+        })
--- www/js/services.js
+++ www/js/services.js
@@ -4,7 +4,23 @@ angular.module('app.services', [])
 
 }])
 
-.service('BlankService', [function(){
-
+.service('PickupPoints', [function(){
+    return {
+        getPickupPoints: function() {
+            return [
+                {
+                    location: "332 6 Ave SE, Calgary, AB T2G 4S6",
+                    stopover: true
+                }, 
+                {
+                    location: "615 Macleod Trail SE, Calgary, AB T2G 4T8",
+                    stopover: true
+                }
+            ];
+        },
+        getDestination: function() {
+            return "Calgary, AB";
+        }
+    };
 }]);
 
