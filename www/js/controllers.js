6b71ff575fc4f1b24a5c4ae123a10b498cb31668
Aryaz <aryaz.egh@gmail.com>
2016-03-05 10:01:40 -0700

    Map added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic'])
 
 .controller('DashCtrl', function($scope) {})
 
@@ -25,4 +25,26 @@ angular.module('starter.controllers', [])
   $scope.settings = {
     enableFriends: true
   };
+})
+
+.controller('MapCtrl', function($scope) {
+    
+})
+
+.directive('map', function() {
+    return {
+        restrict: 'A',
+        link:function(scope, element, attrs){
+
+          var zValue = scope.$eval(attrs.zoom);
+          var lat = scope.$eval(attrs.lat);
+          var lng = scope.$eval(attrs.lng);
+          var myLatlng = new google.maps.LatLng(lat,lng),
+            mapOptions = {
+                zoom: zValue,
+                center: myLatlng
+            },
+            map = new google.maps.Map(element[0],mapOptions);
+        }
+    };
 });
